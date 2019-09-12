00908b89ab9f336b67563da48ef09473c2ed3e89
Tony Jewett <tony@verimour.com>
2019-09-12 19:29:57 -0400

    Linting fixes; updates to lint config; player number selector re-enabled after simulation ends; sim ending handler

--- public/app.js
+++ public/app.js
@@ -1,11 +1,17 @@
-var socket = io();
-var timerFunc;
-socket.on('begin', (data) => {
-	startTimer();
+// eslint-disable-next-line no-undef
+const socket = io();
+let timerFunc;
+socket.on('begin', () => {
+    startTimer();
 });
 
 socket.on('player_finished', (player) => {
-	playerFinished(player);
+    playerFinished(player);
+});
+
+socket.on('finished', (players) => {
+    console.log(players);
+    gameOver(players);
 });
 let beginTime = 0;
 let players = [];
@@ -16,100 +22,107 @@ const timers = document.getElementById('timers');
 createTimers(numPlayers);
 
 document.getElementById('num-players').addEventListener('change', () => {
-	numPlayers = document.getElementById('num-players').value;
-	createTimers(numPlayers);
+    numPlayers = document.getElementById('num-players').value;
+    createTimers(numPlayers);
 });
 
 document.getElementById('btnSimulate').addEventListener('click', () => {
-	document.getElementById('num-players').disabled = true;
-	socket.emit('simulate', { players: players, maxLength: 30 }, (data) => {
-		console.log(data);
-	});
+    document.getElementById('num-players').disabled = true;
+    socket.emit('simulate', { players, maxLength: 5 });
 });
 
 function createTimers(num) {
-	let timerHTML = '';
-	let modalInputs = '';
-	players = [];
-	for (let index = 1; index <= num; index++) {
-		let player = { name: 'Player ' + index, id: `player_${index}`, time: '0:00.000', finished: false };
-		players.push(player);
-		timerHTML += `<h2><label class="player-label" id="label-${index}" for="player_${index}">Player ${index}</label><span class="time" id="player_${index}">0:00.00</span></h2>`;
+    let timerHTML = '';
+    let modalInputs = '';
+    players = [];
+    for (let index = 1; index <= num; index++) {
+        const player = {
+            name: `Player ${index}`,
+            id: `player_${index}`,
+            time: '0:00.000',
+            finished: false
+        };
+        players.push(player);
+        timerHTML += `<h2><label class="player-label" id="label-${index}" for="player_${index}">Player ${index}</label><span class="time" id="player_${index}">0:00.00</span></h2>`;
 
-		modalInputs += `<div class="modal-players"><label class="modal-label" for="edit-${index}">Player ${index}:&nbsp;</label><input type="text" name="edit-${index}" class="modal-textbox" id="edit-${index}"></div>`;
-	}
-	console.log(players);
-	playersRemaining = players.length;
-	timers.innerHTML = timerHTML;
-	document.getElementById('modal-boxes').innerHTML = modalInputs;
-	document.getElementById('modal-save').addEventListener('click', () => {
-		saveNames();
-		modal.style.display = 'none';
-	});
+        modalInputs += `<div class="modal-players"><label class="modal-label" for="edit-${index}">Player ${index}:&nbsp;</label><input type="text" name="edit-${index}" class="modal-textbox" id="edit-${index}"></div>`;
+    }
+    console.log(players);
+    playersRemaining = players.length;
+    timers.innerHTML = timerHTML;
+    document.getElementById('modal-boxes').innerHTML = modalInputs;
+    document.getElementById('modal-save').addEventListener('click', () => {
+        saveNames();
+        modal.style.display = 'none';
+    });
 }
 
 function saveNames() {
-	let index = 0;
-	players.forEach((player) => {
-		let name = document.getElementById(`edit-${index + 1}`).value;
-		if (name != '') {
-			player.name = name;
-			document.getElementById(`label-${index + 1}`).innerText = name;
-		}
-		index++;
-	});
+    let index = 0;
+    players.forEach((player) => {
+        const name = document.getElementById(`edit-${index + 1}`).value;
+        if (name !== '') {
+            player.name = name;
+            document.getElementById(`label-${index + 1}`).innerText = name;
+        }
+        index++;
+    });
 }
 
 function startTimer() {
-	beginTime = new Date(Date.now()).getTime();
-	timerFunc = setInterval(() => {
-		const elapsed = new Date(Date.now()).getTime() - beginTime;
-		const formattedTime = formatTime(elapsed);
-		updateTimes(formattedTime);
-	}, 10);
+    beginTime = new Date(Date.now()).getTime();
+    timerFunc = setInterval(() => {
+        const elapsed = new Date(Date.now()).getTime() - beginTime;
+        const formattedTime = formatTime(elapsed);
+        updateTimes(formattedTime);
+    }, 10);
 }
 
 function updateTimes(newTime) {
-	players.forEach((player) => {
-		if (!player.finished) {
-			player.time = newTime;
-		}
-		drawTime(player);
-	});
+    players.forEach((player) => {
+        if (!player.finished) {
+            player.time = newTime;
+        }
+        drawTime(player);
+    });
 }
 
 function drawTime(player) {
-	document.getElementById(player.id).innerHTML = player.time;
+    document.getElementById(player.id).innerHTML = player.time;
 }
 
 function formatTime(milli) {
-	const minutes = Math.floor(milli / 1000 / 60).toString();
-	const seconds = (Math.floor(milli / 1000) % 60).toString().padStart(2, '0');
-	const ms = (milli % 1000).toString().padStart(3, '0');
-	const time = minutes + ':' + seconds + '.' + ms;
-	return time.slice(0, -1);
+    const minutes = Math.floor(milli / 1000 / 60).toString();
+    const seconds = (Math.floor(milli / 1000) % 60).toString().padStart(2, '0');
+    const ms = (milli % 1000).toString().padStart(3, '0');
+    const time = `${minutes}:${seconds}.${ms}`;
+    return time.slice(0, -1);
 }
 
 function playerFinished(player) {
-	const idx = players.findIndex((x) => {
-		return x.id === player.id;
-	});
-	players[idx] = player;
-	playersRemaining--;
-	if (!playersRemaining) {
-		clearInterval(timerFunc);
-	}
+    const idx = players.findIndex((x) => x.id === player.id);
+    players[idx] = player;
+    playersRemaining--;
+    if (!playersRemaining) {
+        clearInterval(timerFunc);
+    }
 }
 
+function gameOver(donePlayers) {
+    donePlayers.forEach((player) => {
+        drawTime(player);
+    });
+    document.getElementById('num-players').disabled = false;
+}
 // Edit names Modal
-var modal = document.getElementById('myModal');
-var btn = document.getElementById('btnNames');
-var span = document.getElementsByClassName('close')[0];
-btn.onclick = function() {
-	modal.style.display = 'block';
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('btnNames');
+const span = document.getElementsByClassName('close')[0];
+btn.onclick = function showModal() {
+    modal.style.display = 'block';
 };
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = 'none';
-	}
+window.onclick = function hideModal(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 };
