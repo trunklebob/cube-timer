1234ddfcf10c034f7357486d1045be8dac30c514
TonyJewett <tony@verimour.com>
2019-09-19 22:09:05 -0400

    Test

--- public/app.js
+++ public/app.js
@@ -88,7 +88,7 @@ document.getElementById('btnStage').addEventListener('click', () => {
 		player.finished = false;
 		drawTime(player);
 	});
-	io.emit('stage', players);
+	socket.emit('stage', players);
 });
 
 function newGame() {
